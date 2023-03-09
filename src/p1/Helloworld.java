115fee2720517121ff682d23692e6aac8f3c6e46
divyasri <17kokadivya@gmail.com>
2023-03-08 23:42:03 -0800

    pushing divya.txt

--- src/p1/Helloworld.java
+++ src/p1/Helloworld.java
@@ -4,7 +4,7 @@ public class Helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+		System.out.println("Hello world Ibm");
 		int x=5;
 		System.out.println(x);
 		float y=1.2f;
